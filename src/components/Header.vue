5c846fef65cb3350ffc43ade8292c90ccb5bdf97
leroy-netizen <leroythomas051@gmail.com>
2022-01-14 12:06:06 +0300

    Naming|Redirecting|Guarding routes| Auth route guarding

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,20 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">MZIKI</a>
+      <router-link class="text-white font-bold uppercase text-2xl mr-4"
+        :to="{ name: 'Home' }" exact-active-class="no-active">
+        MZIKI
+      </router-link>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <router-link class="px-2 text-white" :to="{ name: 'About' }">
+              About
+            </router-link>
+          </li>
           <li v-if="!userLoggedIn">
             <a
               class="px-2 text-white"
@@ -20,7 +28,9 @@
           </li>
           <template v-else>
           <li>
-            <a class="px-2 text-white" href="#">Manage</a>
+            <router-link class="px-2 text-white" :to="{ name: 'Manage' }">
+              Manage
+            </router-link>
           </li>
           <li>
             <a class="px-2 text-white" href="#" @click.prevent="signout">Logout</a>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,48 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import About from '@/views/About.vue';
+import Manage from '@/views/Manage.vue';
 
 const routes = [
   {
+    name: 'Home',
     path: '/',
     component: Home,
   },
   {
+    name: 'About',
     path: '/about',
     component: About,
   },
-
+  {
+    name: 'Manage',
+    // alias: '/Manage',
+    path: '/manage-music',
+    component: Manage,
+    beforeEnter: (to, from, next) => {
+      console.log('Manage Route Guard');
+      next();
+    },
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'Manage' },
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'Home' },
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
+});
+router.beforeEach((to, from, next) => {
+  console.log('Global Guard');
+
+  next();
 });
 
 export default router;
