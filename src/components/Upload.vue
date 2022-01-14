5d34e1296e53045ef2fd1d589f0225ddd87a2b7e
leroy-netizen <leroythomas051@gmail.com>
2022-01-14 17:42:51 +0300

    working with the progress bars

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -36,33 +36,19 @@
       </div>
       <hr class="my-6" />
       <!-- Progess Bars -->
-      <div class="mb-4">
+      <div class="mb-4" v-for="upload in uploads" :key="upload.name">
         <!-- File Name -->
-        <div class="font-bold text-sm">Just another song.mp3</div>
+        <div class="font-bold text-sm" :class="upload.text">
+            <i :class="upload.icon"> </i>{{ upload.name }}
+        </div>
         <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
           <!-- Inner Progress Bar -->
           <div
-            class="transition-all progress-bar bg-blue-400"
-            style="width: 75%"
-          ></div>
-        </div>
-      </div>
-      <div class="mb-4">
-        <div class="font-bold text-sm">Just another song.mp3</div>
-        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-          <div
-            class="transition-all progress-bar bg-blue-400"
-            style="width: 35%"
-          ></div>
-        </div>
-      </div>
-      <div class="mb-4">
-        <div class="font-bold text-sm">Just another song.mp3</div>
-        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-          <div
-            class="transition-all progress-bar bg-blue-400"
-            style="width: 55%"
-          ></div>
+            class="transition-all progress-bar"
+             :class="upload.variant"
+             :style="{width: upload.current_progress + '%'}"
+             >
+          </div>
         </div>
       </div>
     </div>
@@ -77,6 +63,7 @@ export default {
   data() {
     return {
       is_dragover: false,
+      uploads: [],
     };
   },
   methods: {
@@ -91,7 +78,21 @@ export default {
 
         const storageRef = storage.ref(); // music-app-57bdf.appspot.com
         const songsRef = storageRef.child(`songs/${file.name}`); // music-app-57bdf.appspot.com/example.mp3
-        songsRef.put(file);
+        const task = songsRef.put(file);
+
+        const uploadIndex = this.uploads.push({
+            task,
+            current_progress: 0,
+            name: file.name,
+            variant: 'bg-blue-400',
+            icon:  'fas fa-spinner fa-spin', //font-awesome icon
+            text_class: '',
+        }) - 1;
+
+        task.on('state_changed', (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            this.uploads[uploadIndex].current_progress = progress;
+        });
       });
       console.log(files);
     },
